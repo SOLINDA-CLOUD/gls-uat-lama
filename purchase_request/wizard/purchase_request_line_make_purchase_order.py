57b8784bc7e4fa565fc4174de2323d03ef3ee91e
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-09-12 18:31:29 +0700

    Update purchase request

--- purchase_request/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request/wizard/purchase_request_line_make_purchase_order.py
@@ -50,7 +50,7 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
         for line in self.env["purchase.request.line"].browse(request_line_ids):
             if line.request_id.state == "done":
                 raise UserError(_("The purchase has already been completed."))
-            if line.request_id.state != "approved":
+            if line.request_id.state != "procurement":
                 raise UserError(
                     _("Purchase Request %s is not approved") % line.request_id.name
                 )
