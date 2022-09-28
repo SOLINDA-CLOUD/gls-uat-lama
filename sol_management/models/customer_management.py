8195d8d6394c89be2afcf04de1d0e62ac698ec6d
inicoding <irfannurrahmat@outlook.com>
2022-09-28 14:24:45 +0700

    check error

--- sol_management/models/customer_management.py
+++ sol_management/models/customer_management.py
@@ -201,7 +201,7 @@ class CustomerManagement(models.Model):
 class CustomerAdd(models.Model):
     _inherit = 'res.partner'
 
-    visible_management = fields.Selection(CustomerManagement.point, string='Last Management', compute='_calculate_eval', readonly=True)
+    visible_management_cust = fields.Selection(CustomerManagement.point, string='Last Management', compute='_calculate_eval', readonly=True)
 
     @api.depends()
     def _calculate_eval(self):
@@ -211,7 +211,7 @@ class CustomerAdd(models.Model):
                 ('state', '=', 'approved')
             ])
             if record:
-                rec.visible_management = record.sorted('period_end', reverse=True)[0].final_rate_cust 
+                rec.visible_management_cust = record.sorted('period_end', reverse=True)[0].final_rate_cust 
             else:
-                rec.visible_management = False
+                rec.visible_management_cust = False
 
